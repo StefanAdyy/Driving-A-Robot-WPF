92c6f3756400e4f55b5c3be75700840a8251b3f9
StefanAdyy <stefanmorariu@rocketmail.com>
2024-02-21 10:29:51 +0200

    Added InputOutput View and ViewModel
    Also implemented MainViewModel to hold the urrent ViewModel and updated App.xaml.cs to set the DataContext to the MainViewMode (that contains the current ViewModel)

--- Driving A Robot WPF/Driving A Robot WPF/App.xaml.cs
+++ Driving A Robot WPF/Driving A Robot WPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Driving_A_Robot_WPF.ViewModels;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,16 @@ namespace Driving_A_Robot_WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 
 }
---
+++ Driving A Robot WPF/Driving A Robot WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driving_A_Robot_WPF.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel { get; }
+
+        public MainViewModel() 
+        {
+            CurrentViewModel = new InputOutputViewModel();
+        }
+
+    }
+}
