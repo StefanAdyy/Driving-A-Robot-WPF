066449ae84aff4490f290da6071fa019a084ad5c
StefanAdyy <stefanmorariu@rocketmail.com>
2024-02-21 11:56:19 +0200

    Implemented Range Model

---
+++ Driving A Robot WPF/Driving A Robot WPF/Models/RangeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driving_A_Robot_WPF.Models
+{
+    public class RangeModel
+    {
+        public double MinValue { get; }
+        public double MaxValue { get; }
+
+        public RangeModel(double minValue, double maxValue)
+        {
+            if (minValue > maxValue)
+            {
+                throw new ArgumentException("MinValue must be less than or equal to MaxValue.");
+            }
+
+            MinValue = minValue; 
+            MaxValue = maxValue;
+        }
+
+    }
+}
--- Driving A Robot WPF/Driving A Robot WPF/obj/Debug/net6.0-windows/Driving A Robot WPF.AssemblyInfo.cs
+++ Driving A Robot WPF/Driving A Robot WPF/obj/Debug/net6.0-windows/Driving A Robot WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Driving A Robot WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36975b741c0ac201926ccb8ebc2b8304f3d6d1dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c6f3756400e4f55b5c3be75700840a8251b3f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Driving A Robot WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Driving A Robot WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
