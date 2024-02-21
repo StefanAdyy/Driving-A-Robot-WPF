a9a467d2abd14bdff574f1754edfb14aef2715ae
StefanAdyy <stefanmorariu@rocketmail.com>
2024-02-21 22:57:57 +0200

    Added Input output Properties + Binding
    (Input binding will change in a future update to bind to a command)

--- Driving A Robot WPF/Driving A Robot WPF/ViewModels/InputOutputViewModel.cs
+++ Driving A Robot WPF/Driving A Robot WPF/ViewModels/InputOutputViewModel.cs
@@ -8,5 +8,33 @@ namespace Driving_A_Robot_WPF.ViewModels
 {
     public class InputOutputViewModel : ViewModelBase
     {
-    }
+		private string _input;
+		public string Input
+		{
+			get
+			{
+				return _input;
+			}
+			set
+			{
+				_input = value;
+				//Output = value;
+				OnPropertyChanged(nameof(Input));
+			}
+		}
+
+		private string _output;
+		public string Output
+		{
+			get
+			{
+				return _output;
+			}
+			set
+			{
+				_output = value;
+				OnPropertyChanged(nameof(Output));
+			}
+		}
+	}
 }
