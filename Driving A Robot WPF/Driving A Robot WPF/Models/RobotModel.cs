63770e5e100d4d9dcd9c433b70a97a15453da5c8
StefanAdyy <stefanmorariu@rocketmail.com>
2024-02-21 11:58:04 +0200

    Implemented Robot Model
    Also overridden "ToString()" method to print the robot coordinates

---
+++ Driving A Robot WPF/Driving A Robot WPF/Models/RobotModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Driving_A_Robot_WPF.Models
+{
+    public class Robot
+    {
+        private double _xCoordinate;
+        private double _yCoordinate;
+        private double _zCoordinate;
+
+        public double XCoordinate
+        {
+            get { return _xCoordinate; }
+            set
+            {
+                _xCoordinate = value;
+                AreCoordinatesSet = true; 
+            }
+        }
+
+        public double YCoordinate
+        {
+            get { return _yCoordinate; }
+            set
+            {
+                _yCoordinate = value;
+                AreCoordinatesSet = true;
+            }
+        }
+
+        public double ZCoordinate
+        {
+            get { return _zCoordinate; }
+            set
+            {
+                _zCoordinate = value;
+                AreCoordinatesSet = true;
+            }
+        }
+
+        public bool AreCoordinatesSet { get; private set; }
+
+        public Robot(double xCoordinate, double yCoordinate, double zCoordinate)
+        {
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+            ZCoordinate = zCoordinate;
+            AreCoordinatesSet = true;
+        }
+
+        public override string ToString()
+        {
+            string s;
+            
+            if (AreCoordinatesSet)
+            {
+                s = XCoordinate.ToString() + " " + YCoordinate.ToString() + " " + ZCoordinate.ToString();
+            }
+            else
+            {
+                s = "??? ??? ???";   
+            }
+
+            return s;
+        }
+    }
+}
